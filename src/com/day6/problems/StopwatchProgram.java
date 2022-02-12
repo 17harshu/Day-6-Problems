3da0045b4d47b11d25f696169b753164f2b103e0
17harshu <harshalanandas17@gmail.com>
2022-02-12 16:47:41 +0530

    added: Day6 Problems

--- src/com/day6/problems/StopwatchProgram.java
+++ src/com/day6/problems/StopwatchProgram.java
@@ -1,4 +1,21 @@
 package com.day6.problems;
 
+import java.util.Scanner;
+
 public class StopwatchProgram {
+	public static void elapsedTime() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter 's' to start the stopwatch");
+		char start = sc.next().charAt(0);
+
+		System.out.println("Enter 'e' to end the stopwatch");
+		char end = sc.next().charAt(0);
+
+		double time = (double) (end - start) / 1000;
+		System.out.println("Time Elapsed: " + time + " seconds");
+	}
+
+	public static void main(String[] args) {
+		elapsedTime();
+	}
 }
